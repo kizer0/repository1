43e8ac3ee2a3565db931856159d0b2b134f305c3
kizer0 <lapudidamian@icloud.com>
2024-08-15 22:10:30 +0400

    tinkoffcalculator

---
+++ TinkoffCalculator/TinkoffCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TinkoffCalculator
+//
+//  Created by Lapudi Damian on 15.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
